8b5bbdd93a64f4e952c2f84c8649f5925815d8bf
Sławek <slawek.4363@gmail.com>
2022-10-12 08:34:27 +0200

    2

--- app/src/main/java/com/enzo/foodaplication/manager/impl/DishesManagerImpl.kt
+++ app/src/main/java/com/enzo/foodaplication/manager/impl/DishesManagerImpl.kt
@@ -1,4 +1,32 @@
 package com.enzo.foodaplication.manager.impl
 
-class DishesManagerImpl {
+import com.enzo.foodaplication.data.model.Dishes
+import com.enzo.foodaplication.manager.DishesManager
+
+class DishesManagerImpl : DishesManager {
+
+
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+
+    override fun add(dish: Dishes) {
+        TODO("Not yet implemented")
+    }
+
+    override fun set(index: Int, dish: Dishes) {
+        TODO("Not yet implemented")
+    }
+
+    override fun get(index: Int): Dishes {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAll(): List<Dishes> {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(index: Int) {
+        TODO("Not yet implemented")
+    }
 }
