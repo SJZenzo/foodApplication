4e2460da2de2ef16b429f1f6c84c3cb0567c3c0e
Enzo <slawek.4363@gmail.com>
2023-01-03 20:03:11 +0100

    tez dziala

--- app/src/main/java/com/enzo/foodaplication/ui/dishesList/DishesListFragment.kt
+++ app/src/main/java/com/enzo/foodaplication/ui/dishesList/DishesListFragment.kt
@@ -2,7 +2,6 @@ package com.enzo.foodaplication.ui.dishesList
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.enzo.foodaplication.ui.dishesDetails.DishesDetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
